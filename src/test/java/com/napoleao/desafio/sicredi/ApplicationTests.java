49254b7ac801cde3119933b31081ea26c275f114
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2022-09-24 11:56:18 -0300

    Subindo projeto base

---
+++ src/test/java/com/napoleao/desafio/sicredi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.napoleao.desafio.sicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
