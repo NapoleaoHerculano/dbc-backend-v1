a28290e8a513cf4450b245fae3165007b91dfd9f
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2022-09-24 13:22:59 -0300

    Criacao da classe base que representa uma sessao de votacao.

---
+++ src/main/java/com/napoleao/desafio/sicredi/modelos/Sessao.java
@@ -0,0 +1,22 @@
+package com.napoleao.desafio.sicredi.modelos;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Sessao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime dataDeAbertura;
+    private LocalDateTime dataDeFechamento;
+
+}
