6e4d9562d0c6489ab3c49ce3caf58e2fcb8d2733
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2022-09-25 10:51:06 -0300

    Adicionando validacoes no formulario de associados

--- src/main/java/com/napoleao/desafio/sicredi/formularios/AssociadoForm.java
+++ src/main/java/com/napoleao/desafio/sicredi/formularios/AssociadoForm.java
@@ -2,11 +2,18 @@ package com.napoleao.desafio.sicredi.formularios;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class AssociadoForm {
 
+    @NotBlank
     private String nome;
+
+    @NotBlank
+    @CPF
     private String cpf;
 }
