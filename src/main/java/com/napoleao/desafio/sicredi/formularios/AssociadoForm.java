6846812e9f570af81d687c1363f52e71fe81dcaa
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2022-09-25 09:10:58 -0300

    Criacao da classe que representa o cadastro de um associado

---
+++ src/main/java/com/napoleao/desafio/sicredi/formularios/AssociadoForm.java
@@ -0,0 +1,12 @@
+package com.napoleao.desafio.sicredi.formularios;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AssociadoForm {
+
+    private String nome;
+    private String cpf;
+}
