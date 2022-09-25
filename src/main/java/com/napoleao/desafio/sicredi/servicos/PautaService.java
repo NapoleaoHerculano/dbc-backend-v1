66761285d87ce97166ccdf8b9c2073e051d11287
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2022-09-25 18:04:34 -0300

    Implementacao do metodo que busca uma pauta pelo id

--- src/main/java/com/napoleao/desafio/sicredi/servicos/PautaService.java
+++ src/main/java/com/napoleao/desafio/sicredi/servicos/PautaService.java
@@ -2,12 +2,15 @@ package com.napoleao.desafio.sicredi.servicos;
 
 import com.napoleao.desafio.sicredi.dtos.PautaDto;
 import com.napoleao.desafio.sicredi.excecoes.AssociadoNaoEncontradoException;
+import com.napoleao.desafio.sicredi.excecoes.PautaNaoEncontradaException;
 import com.napoleao.desafio.sicredi.formularios.PautaForm;
 import com.napoleao.desafio.sicredi.modelos.Associado;
 import com.napoleao.desafio.sicredi.modelos.Pauta;
 import com.napoleao.desafio.sicredi.repositorios.PautaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PautaService {
@@ -32,6 +35,14 @@ public class PautaService {
 
         return new PautaDto(pauta);
 
+    }
+
+    public Pauta buscarPautaPeloId(Long id) throws PautaNaoEncontradaException {
+        Optional<Pauta> pautaOptional = pautaRepository.findById(id);
+        if (!pautaOptional.isPresent()){
+            throw new PautaNaoEncontradaException();
+        }
+        return pautaOptional.get();
     }
 
     private String recuperarTokenDoHeader(String token){
