f8f3be807d84e2b9dd84ca2ecd721731325c5cd6
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2022-09-25 18:10:51 -0300

    Implementacao do servico e controlador responsavel por abrir uma sessao

---
+++ src/main/java/com/napoleao/desafio/sicredi/controladores/SessaoController.java
@@ -0,0 +1,31 @@
+package com.napoleao.desafio.sicredi.controladores;
+
+import com.napoleao.desafio.sicredi.dtos.SessaoDto;
+import com.napoleao.desafio.sicredi.excecoes.AssociadoNaoEncontradoException;
+import com.napoleao.desafio.sicredi.excecoes.PautaNaoEncontradaException;
+import com.napoleao.desafio.sicredi.formularios.SessaoForm;
+import com.napoleao.desafio.sicredi.servicos.SessaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/v1/sessao")
+public class SessaoController {
+
+    @Autowired
+    private SessaoService sessaoService;
+
+    @PostMapping("/abertura/pauta/{idPauta}")
+    public ResponseEntity<SessaoDto> abrirSessaoDeVotacao(@RequestHeader("Authorization") String token, @PathVariable Long idPauta, @RequestBody(required = false) @Valid SessaoForm sessaoForm){
+        try {
+            return new ResponseEntity<>(sessaoService.abrirSessaoDeVotacao(token, idPauta, sessaoForm), HttpStatus.CREATED);
+        } catch (AssociadoNaoEncontradoException | PautaNaoEncontradaException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/com/napoleao/desafio/sicredi/servicos/SessaoService.java
@@ -0,0 +1,56 @@
+package com.napoleao.desafio.sicredi.servicos;
+
+import com.napoleao.desafio.sicredi.dtos.SessaoDto;
+import com.napoleao.desafio.sicredi.excecoes.AssociadoNaoEncontradoException;
+import com.napoleao.desafio.sicredi.excecoes.PautaNaoEncontradaException;
+import com.napoleao.desafio.sicredi.formularios.SessaoForm;
+import com.napoleao.desafio.sicredi.modelos.Associado;
+import com.napoleao.desafio.sicredi.modelos.Pauta;
+import com.napoleao.desafio.sicredi.modelos.Sessao;
+import com.napoleao.desafio.sicredi.repositorios.SessaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class SessaoService {
+
+    @Autowired
+    private SessaoRepository sessaoRepository;
+
+    @Autowired
+    private PautaService pautaService;
+
+    @Autowired
+    private AssociadoService associadoService;
+
+    @Autowired
+    private TokenService tokenService;
+
+    public SessaoDto abrirSessaoDeVotacao(String token, Long idPauta, SessaoForm sessaoForm) throws AssociadoNaoEncontradoException, PautaNaoEncontradaException {
+        Long idAssociado = tokenService.getUserIdInToken(recuperarTokenDoHeader(token));
+        Associado associado = associadoService.findUserById(idAssociado);
+
+        Pauta pauta = pautaService.buscarPautaPeloId(idPauta);
+
+        Sessao sessao = new Sessao();
+        sessao.setAssociado(associado);
+        sessao.setPauta(pauta);
+        sessao.setDataDeAbertura(LocalDateTime.now());
+        if (sessaoForm == null){
+            sessao.setDataDeFechamento(LocalDateTime.now().plusMinutes(1));
+        }else {
+            sessao.setDataDeFechamento(LocalDateTime.now().plusMinutes(sessaoForm.getDuracaoDaSessao()));
+        }
+
+        sessaoRepository.save(sessao);
+
+        return new SessaoDto(sessao);
+    }
+
+    private String recuperarTokenDoHeader(String token){
+        return token.substring(7);
+    }
+
+}
